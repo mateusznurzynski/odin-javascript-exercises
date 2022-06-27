857a2f2c6ceb453e31a86ec24a29e64aabd6b263
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-06-27 16:51:46 +0200

    Finish exercise number 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const stringArray = Array.from(string);
+  const reversedArray = stringArray.reverse();
+  const output = reversedArray.join('');
+  return output;
 };
 
 // Do not edit below this line
