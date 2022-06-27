2f0ae210cc786954c9ccf41bf74839f9cdbb9e5f
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-06-27 17:49:35 +0200

    Finished exercise number 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  let celsius = ((fahrenheit - 32) * 5) / 9;
+  let stringCelsius = celsius + '';
+  const output = stringCelsius.includes('.') ? celsius.toFixed(1) : celsius;
+  return +output;
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  let stringFahrenheit = fahrenheit + '';
+  const output = stringFahrenheit.includes('.')
+    ? fahrenheit.toFixed(1)
+    : fahrenheit;
+  return +output;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
