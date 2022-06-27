7f0e4f18b60ced918449a2e74fa6a45fa4cb303a
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-06-27 17:30:11 +0200

    Finish exercise numer 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let leapYear = false;
+  if (year % 100 === 0) {
+    leapYear = year % 400 === 0 ? true : false;
+  } else {
+    leapYear = year % 4 === 0 ? true : false;
+  }
+  return leapYear;
 };
 
 // Do not edit below this line
