4e32d881782069da842a432e2c838e3bb824dc66
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-07-02 20:56:13 +0200

    Finish exercise number 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (a, b) {
+  const output = a + b;
+  return output;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  const output = a - b;
+  return output;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  const output = numbers.reduce((sum, e) => {
+    sum += e;
+    return sum;
+  }, 0);
+  return output;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  const output = numbers.reduce((current, e) => {
+    current *= e;
+    return current;
+  }, 1);
+  return output;
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+  const output = number ** power;
+  return output;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let sum = 1;
+  for (let i = 1; i <= number; i++) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
@@ -29,5 +44,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
