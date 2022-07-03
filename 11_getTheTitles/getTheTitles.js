41d4d9398649d251d6430f2e30e0345fb941ed7a
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-07-03 18:35:06 +0200

    Finish exercise number 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (bookObj) {
+  const output = [];
+  bookObj.forEach((e) => {
+    output.push(e.title);
+  });
+  return output;
 };
 
 // Do not edit below this line
