c813979b5f1669007e5cd6599fa9a5a35daba11c
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-07-03 19:00:43 +0200

    Finish exercise number 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentDate = new Date();
+  const highest = people.reduce(
+    (highest, e) => {
+      const birth = e.yearOfBirth;
+      let death = null;
+      if (e.yearOfDeath) {
+        death = e.yearOfDeath;
+      } else {
+        death = currentDate.getFullYear();
+      }
+      const years = death - birth;
+      e.years = years;
+      if (e.years > highest.years) {
+        return e;
+      }
+      return highest;
+    },
+    { years: 0 }
+  );
 
+  return highest;
 };
 
 // Do not edit below this line
