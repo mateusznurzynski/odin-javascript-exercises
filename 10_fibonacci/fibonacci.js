9feab5b984a28f918fc41fabec8a9929be144576
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-07-03 18:27:16 +0200

    Finish exercise number 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (total) {
+  total = +total;
+  if (total <= 0) {
+    return 'OOPS';
+  }
 
+  let previousNumber = 1;
+  let currentNumber = 1;
+
+  const finalArray = [previousNumber, currentNumber];
+  if (finalArray.length >= total) {
+    return finalArray[total - 1];
+  }
+  while (finalArray.length !== total) {
+    const newCurrentNumber = previousNumber + currentNumber;
+    const newPreviousNumber = currentNumber;
+    finalArray.push(newCurrentNumber);
+    previousNumber = newPreviousNumber;
+    currentNumber = newCurrentNumber;
+  }
+  return finalArray[total - 1];
 };
 
 // Do not edit below this line
