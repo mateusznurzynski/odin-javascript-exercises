5eab46293f1c980d6c7d65ee1b821ee64ec6c96d
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-07-03 18:11:53 +0200

    Finish exercise number 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.toLowerCase();
+  const invalidCharacters = ['!', ' ', ',', '.'];
+  const clearString = Array.from(string).filter((e) => {
+    if (!invalidCharacters.includes(e)) {
+      return e;
+    }
+  });
+  const reversedArray = [...clearString].reverse();
+  let index = 0;
+  if (
+    clearString.every((e) => {
+      if (e === reversedArray[index]) {
+        index++;
+        return true;
+      }
+    })
+  ) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
